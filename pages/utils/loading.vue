600cf90bf2c4f406c2637c064ea8443121e545c8
강준성 <jskang@wisenut.co.kr>
2023-04-17 18:19:44 +0900

    refactor: 로딩 및 프로그래스바 컴포넌트 변경

--- pages/utils/loading.vue
+++ pages/utils/loading.vue
@@ -5,50 +5,49 @@
         <div>
             <fieldset>
                 <legend>Size 50px</legend>
-
-                <loading-type1 class="loading-size-50"></loading-type1>
-                <loading-type2 class="loading-size-50"></loading-type2>
-                <loading-type3 class="loading-size-50"></loading-type3>
+                <va-progress-circle :size="50" :thickness="0.1" v-model="percent" style="display: inline-block; margin-right: 30px;">{{percent}}</va-progress-circle>
+                <va-progress-circle :size="50" :thickness="0.3" indeterminate style="display: inline-block; margin-right: 30px;"/>
+                <va-progress-circle :size="50" :thickness="0.7" indeterminate style="display: inline-block; margin-right: 30px;">Loading</va-progress-circle>
             </fieldset>
 
             <fieldset>
                 <legend>Size 100px</legend>
-
-                <loading-type1 class="loading-size-100"></loading-type1>
-                <loading-type2 class="loading-size-100"></loading-type2>
-                <loading-type3 class="loading-size-100"></loading-type3>
+                <va-progress-circle :size="100" :thickness="0.1" indeterminate style="display: inline-block; margin-right: 30px;"/>
+                <va-progress-circle :size="100" :thickness="0.3" indeterminate style="display: inline-block; margin-right: 30px;">Loading</va-progress-circle>
+                <va-progress-circle :size="100" :thickness="0.7" v-model="percent" style="display: inline-block; margin-right: 30px;">{{percent}}</va-progress-circle>
             </fieldset>
 
             <fieldset>
                 <legend>Size 200px</legend>
+                <va-progress-circle :size="200" :thickness="0.1" indeterminate style="display: inline-block; margin-right: 30px;">Loading</va-progress-circle>
+                <va-progress-circle :size="200" :thickness="0.3" v-model="percent" style="display: inline-block; margin-right: 30px;">{{percent}}</va-progress-circle>
+                <va-progress-circle :size="200" :thickness="0.7" indeterminate style="display: inline-block; margin-right: 30px;"/>
+            </fieldset>
 
-                <loading-type1 class="loading-size-200"></loading-type1>
-                <loading-type2 class="loading-size-200"></loading-type2>
-                <loading-type3 class="loading-size-200"></loading-type3>
+            <fieldset>
+                <legend>Progress Bar</legend> <br/>
+                <va-progress-bar indeterminate/> <br/>
+                <va-progress-bar v-model="percent" color="danger"/> <br/>
+                <va-progress-bar color="#a1a1a1"/> <br/>
             </fieldset>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+const percent: globalThis.Ref<number> = ref(0);
+setInterval( () => {
+    percent.value = percent.value + Math.ceil(new Date().getTime() % 20) + 7;
+    if (percent.value > 100) {
+        percent.value = 0;
+    }
+}, 2000);
 </script>
 
 <style scoped>
 h1 {
     font-size: 20pt;
     margin-bottom: 20px;
-}
-.loading-size-50 {
-    width: 50px;
-    display: inline-block;
-}
-.loading-size-100 {
-    width: 100px;
-    display: inline-block;
-}
-.loading-size-200 {
-    width: 200px;
-    display: inline-block;
 }
 legend {
     font-weight: bold;
