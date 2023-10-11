46fd69984cd0b9cfd305ee8793cc359e67f2e17c
권호중 <hjkwon@wisenut.co.kr>
2023-10-11 16:28:06 +0900

    fix: api server 주소 변경

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   title: 'Hello Nuxt!!!!',
   logoTitle: 'MLOps Platform',
-  apiServer: 'https://wise-mlops.apps.wisenut.dp:30443',
+  apiServer: 'http://af283297f6b68471d8ddc32c7f4c1459-989288742.ap-northeast-2.elb.amazonaws.com:8000',
   apiToken: '',
   theme: {
     dark: true,
