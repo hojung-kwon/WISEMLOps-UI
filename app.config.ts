ce82fe13cad13d6589ab48e4fff08372e41bc5c0
junhyeok <junwork@wisenut.co.kr>
2023-10-27 16:09:52 +0900

    fix:backend api url 변경

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   title: 'Hello Nuxt!!!!',
   logoTitle: 'MLOps Platform',
-  apiServer: 'http://a771cb26eb4174db69190d07a3138614-1030543049.ap-northeast-2.elb.amazonaws.com:8000',
+  apiServer: 'http://a902a21edcd0c4b169b02d4284661c3f-92343453.ap-northeast-2.elb.amazonaws.com:8000',
   apiToken: '',
   theme: {
     dark: true,
